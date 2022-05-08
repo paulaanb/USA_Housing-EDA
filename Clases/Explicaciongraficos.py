5b797f72d32e0d836a1f6258c2e950b833dd56ee
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-05-08 19:10:22 +0200

    explicacion de las graficas

---
+++ Clases/Explicaciongraficos.py
@@ -0,0 +1,14 @@
+#En este archivo vamos a explicar los graficos obtenidos
+
+Se generan los histogramas:
+    -Histograma_de_media_anterior_casa.png muestra la antiguedad de las viviendas
+    -Histograma_de_media_numero_dormitorios_casas.png muestra la distribución de los dormitorios por viviendas
+    -Histograma_de_media_numero_habitaciones.png muestra la distribución de las habitaciones por vivienda
+    -Histograma_de_media_salario.png muestra la distribución del salario 
+    -Histograma_de_poblacion.png  muestra la distribución de la población
+    -Histograma_de_precio.png  muestra la distribución de precios
+    -Incremento_anual.png muestra el incremento anual en porcentaje del número de viviendas
+    
+Teniendo en cuenta todo lo anterior, podemos apreciar que las variables numéricas tienen una distribución simétrica, asemejandose a la tan famosa campana de Gauss.
+Esto se aprecia en el grafico Histograma_de_media_salario
+Los demas graficos no presentan esta simetria, habiendo cada vez menos datos repetidos
