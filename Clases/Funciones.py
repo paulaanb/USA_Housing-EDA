d601815e4ccca0b82d98d513dffa5f1ea3ba3091
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-05-08 15:36:52 +0200

    Cambio de idioma en las columnas del cvs

--- Clases/Funciones.py
+++ Clases/Funciones.py
@@ -2,3 +2,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
+#Nos disponemos a leer el archivo csv
+df = pd.read_csv('USA_Housing.csv', sep = ',')
+print("El dataset inicial es el siguiente :")
+print(df, "\n")
+
+#Como los nombres de las columnas estan en ingles, vamos a traducirlos a español para poder hacer un mejor analisis
+#Las columnas iniciales del csv son:
+print('-------------Columnas del CSV-----------')
+print(df.columns, "\n")
+#Ahora lo cambiamos
+print("Realizamos el cambio de ingles a español:")
+cambioingesp =  {'Avg. Area Income': 'Ganancia Media','Avg. Area House Age':'Edad Casa Media', 'Avg. Area Number of Rooms':'Num Habitaciones Medio', 'Avg. Area Number of Bedrooms': 'Num HabitCama Medio', 'Area Population': 'Poblacion en Area', 'Price':'Precio', 'Address':'Direccion'}
+df2 = traduccion(f, traducido)
+print("\n")
